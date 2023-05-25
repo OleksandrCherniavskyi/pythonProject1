e5e6e121ceb833935a67c5e9cc8f2047f1f7af96
OleksandrCherniavskyi <105165580+OleksandrCherniavskyi@users.noreply.github.com>
2023-05-25 12:38:26 +0200

    connect DB change time zone

--- my_page/my_page/settings.py
+++ my_page/my_page/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'my_page.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'C:/Users/bootbyt/PycharmProjects/pythonProject1/justjoin.sqlite3',
     }
 }
 
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Warsaw'
 
 USE_I18N = True
 
