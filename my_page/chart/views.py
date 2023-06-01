5ed0e77bf51dde1593e35be572cd599cc12d6bf7
OleksandrCherniavskyi <105165580+OleksandrCherniavskyi@users.noreply.github.com>
2023-06-01 21:31:54 +0200

    next temp

--- my_page/chart/views.py
+++ my_page/chart/views.py
@@ -8,11 +8,11 @@ import datetime
 
 
 seven_days_ago = now() - datetime.timedelta(days=7)
-
+month_ago = now() - datetime.timedelta(days=30)
 
 def main(request):
 
-    top_offers = Offers.objects.filter(published_at__gte=seven_days_ago) \
+    top_offers = Offers.objects.filter(published_at__gte=month_ago) \
                      .values('title') \
                      .annotate(title_count=Count('title')) \
                      .order_by('-title_count')[:7]
@@ -21,12 +21,12 @@ def main(request):
                      .annotate(skill_count=Count('name'), avg_level=Avg('level')) \
                      .order_by('-skill_count')[:7]
 
-    top_city = Offers.objects.filter(published_at__gte=seven_days_ago) \
+    top_city = Offers.objects.filter(published_at__gte=month_ago) \
                      .values('city') \
                      .annotate(city_count=Count('city')) \
                      .order_by('-city_count')[:7]
 
-    city_to_work_on_site = Offers.objects.filter(published_at__gte=seven_days_ago,
+    city_to_work_on_site = Offers.objects.filter(published_at__gte=month_ago,
                                                  workplace_type__in=['partly_remote', 'office']) \
                                .values('city') \
                                .annotate(city_count=Count('city')) \
--- my_page/static/js/main.js
+++ my_page/static/js/main.js
@@ -1,13 +1,25 @@
+
 function toggleContent(header) {
   const container = header.parentNode;
   const content = container.querySelector('.content');
-  const expandIcon = container.querySelector('.expand-icon');
-
-  if (content.style.display === 'none') {
+  const expandIcon = container.querySelector('.expand-icon')
+  // Check if the content is currently visible
+  const isContentVisible = content.style.display !== 'none'
+  // Hide all other content elements except the clicked one
+  const allContainers = document.querySelectorAll('.container');
+  allContainers.forEach((container) => {
+    const content = container.querySelector('.content');
+    const expandIcon = container.querySelector('.expand-icon')
+    if (container !== header.parentNode) {
+      content.style.display = 'none';
+      expandIcon.innerText = '+';
+    }
+  })
+  // Toggle the visibility of the clicked container
+  if (!isContentVisible) {
     content.style.display = 'block';
     expandIcon.innerText = '-';
   } else {
     content.style.display = 'none';
     expandIcon.innerText = '+';
   }
-}
