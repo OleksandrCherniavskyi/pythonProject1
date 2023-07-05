f290f92eb937e06a407457cfaadb4ed5643fcf68
OleksandrCherniavskyi <105165580+OleksandrCherniavskyi@users.noreply.github.com>
2023-07-05 09:11:25 +0200

    update Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -13,4 +13,4 @@ RUN pip3 install -r requirements.txt
 COPY . .
 
 # Specify the command to run your Django app
-CMD ["python", "my_page/manage.py", "runserver", "0.0.0.0:8000"]
+CMD ["python", "my_page/manage.py", "runserver", "0.0.0.0:8080"]
--- my_page/my_page/settings.py
+++ my_page/my_page/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^w7jg-g2_afyoe^5g@5%%&1gd@sye(3e*i$z%ur^=$@ka=sr%5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
