2e31bbb404f7dc3788aaf0e726f69facb0cb2947
OleksandrCherniavskyi <105165580+OleksandrCherniavskyi@users.noreply.github.com>
2023-08-10 10:13:14 +0200

    - return local DB

---
+++ git_update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Change directory to your project directory
+cd /project/home/oleksandrcherniavskyi/workspace
+
+# Fetch and merge changes from origin/master
+git fetch
+git merge origin/master
